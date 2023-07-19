b337d37d9a560771f56eccab44d34a2952f39ee6
yuki-fujisaki <yuki.f.bizz@gmail.com>
2023-07-19 23:31:27 +0900

    [mod] modelを使ってdbから値を取得、保存ができるようにした

--- pkg/controller/user.go
+++ pkg/controller/user.go
@@ -4,17 +4,48 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"golang_mvc_sample/pkg/model"
 	"golang_mvc_sample/pkg/view"
 )
 
 func GetAllUsers(w http.ResponseWriter, r *http.Request) {
-	users := []view.User{}
+	users, err := model.GetAllUsers()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	viewUsers := make([]view.User, len(users))
+	for i, user := range users {
+		viewUsers[i] = view.User{
+			ID:    user.ID,
+			Name:  user.Name,
+			Email: user.Email,
+		}
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(users)
+	json.NewEncoder(w).Encode(viewUsers)
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	user := view.User{}
+	var viewUser view.User
+	err := json.NewDecoder(r.Body).Decode(&viewUser)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	user := model.User{
+		Name:  viewUser.Name,
+		Email: viewUser.Email,
+	}
+	err = model.CreateUser(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(user)
+	json.NewEncoder(w).Encode(viewUser)
 }
