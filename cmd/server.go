4300036d49b436d06abe29fe30a1e306e23f03cb
yuki-fujisaki <yuki.f.bizz@gmail.com>
2023-07-19 21:00:34 +0900

    [add] router追加

--- cmd/server.go
+++ cmd/server.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+	"golang_mvc_sample/pkg/controller"
 	"net/http"
 )
 
 func main() {
+	r := controller.UserRoutes()
+
 	fmt.Println("Server running at http://localhost:3000")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
